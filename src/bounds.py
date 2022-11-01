e5c17c69ef8a9234c12149a754fcc01841073d08
Sara-Elna <elna.elisa@gmail.com>
2022-11-01 19:10:24 +0100

    bounds attempt 6

--- src/bounds.py
+++ src/bounds.py
@@ -11,48 +11,48 @@ def lower_bound(x: list[int], v: int) -> int:
 
     If all values in x are smaller than v, return len(x).
 
-    >>> lower_bound([1, 4, 7, 9, 20, 24, 697], 20)
-    4
+    >>> lower_bound([4, 4, 7, 7, 9, 10, 20, 20, 20], 20)
+    6
+
+    >>> lowe_bound([1, 2, 5], 10)
+    3
 
     """
     start = 0 # start of search interval
     end = len(x) # end of search interval
     middel = start + ((end - start)//2) # middel of search interval
 
-    while (end-start) >= 1 and middel < len(x):
-        if x[middel] > v:
-            end = middel # new interval is cut in half
-            middel = start + ((end - start)//2) # middel of new interval is found
-        elif x[middel] < v:
-            start = middel + 1 # new interval is cut in half, +1 because python is start inclusive
-            middel = start + ((end - start)//2)
-        elif x[middel] == v:
-            return middel
-    else:
-        return len(x)
+    while start < end:
+        middel = start + ((end - start)//2) # middel of interval
+        if x[middel] >= v:
+            end = middel + 1
+        else:
+            start = middel
+    return middel
 
+# Previous verison did not work as I intended,
+# got inspiration from https://www.youtube.com/watch?v=6-15eccc6ek&ab_channel=CodeSavant
 
 def upper_bound(x: list[int], v: int) -> int:
     """Get the index of the upper bound of v in x.
 
     If all values in x are smaller than v, return len(x).
 
-    >>> upper_bound([1, 4, 7, 9, 20, 24, 697], 20)
-    4
+    >>> upper_bound([4, 4, 7, 7, 9, 10, 20, 20, 20], 7)
+    3
+
+    >>> upper_bound([1, 2, 5], 10)
+    3
 
     """
     start = 0 # start of search interval
     end = len(x) # end of search interval
     middel = start + ((end - start)//2) # middel of search interval
 
-    while (end-start) >= 1 and middel < len(x):
-        if x[middel] > v:
-            end = middel # new interval is cut in half
-            middel = start + ((end - start)//2) # middel of new interval is found
-        elif x[middel] < v:
-            start = middel + 1 # new interval is cut in half, +1 because python is start inclusive
-            middel = start + ((end - start)//2)
-        elif x[middel] == v:
-            return middel
-    else:
-        return len(x)
+    while start < end:
+        middel = start + ((end - start)//2) # middel of interval
+        if x[middel] <= v:
+            start = middel + 1
+        else:
+            end = middel
+    return middel
